e3470f6a0329aeb6759fd283f5b8ab23a582c996
Sanket Yelkar <sanket.yelkar@thinkitive.com>
2022-06-17 19:43:07 +0530

    Refactor

---
+++ src/main/java/com/example/vault/resources/VaultResource.java
@@ -0,0 +1,46 @@
+package com.example.vault.resources;
+
+import com.example.vault.dto.Credentials;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultResponse;
+import org.springframework.vault.support.VaultResponseSupport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.URISyntaxException;
+
+@RestController
+public class VaultResource {
+
+//    @Autowired
+//    VaultConfiguration vaultConfiguration;
+
+    @Autowired
+    VaultTemplate vaultTemplate;
+
+    @PostMapping("/store")
+    public String store(@RequestBody Credentials credentials) {
+        //        VaultTemplate vaultTemplate = vaultConfiguration.vaultTemplate();
+
+        VaultResponse response =
+                vaultTemplate.write("cubbyhole/googlefit", new Credentials("sajdsf123", "asdqwe343sdasdqwe343sd"));
+
+        return "Success";
+
+    }
+
+    @GetMapping("/retrieve")
+    public Credentials receive() {
+
+        VaultResponseSupport<Credentials> resp = vaultTemplate.read("cubbyhole/googlefit", Credentials.class);
+
+        if (resp == null) {
+            return new Credentials("", "");
+        }
+
+        return resp.getData();
+    }
+
+}
